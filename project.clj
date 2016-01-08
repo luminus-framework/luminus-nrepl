f63d064dbe4627902052358d630ac4b9325f641c
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-08 09:07:17 -0500

    updated project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject luminus-repl "0.1.0"
-  :description "REPL server for Luminus"
-  :url "https://github.com/luminus-framework/luminus-repl"
+(defproject luminus-nrepl "0.1.0"
+  :description "nREPL server for Luminus"
+  :url "https://github.com/luminus-framework/luminus-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
