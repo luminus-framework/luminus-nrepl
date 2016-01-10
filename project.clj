6bc5b38ecd463cd17e3d9ee718c2ead1b2020262
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-10 16:51:54 -0500

    cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-nrepl "0.1.0"
+(defproject luminus-nrepl "0.1.1"
   :description "nREPL server for Luminus"
   :url "https://github.com/luminus-framework/luminus-nrepl"
   :license {:name "Eclipse Public License"
--- src/luminus/repl_server.clj
+++ src/luminus/repl_server.clj
@@ -11,16 +11,21 @@
     (log/info "nREPL server stopped")))
 
 (defn start
-  "Start a network repl for debugging on specified port.
-  :bind, :transport-fn, :handler, :ack-port and :greeting-fn will be forwarded to clojure.tools.nrepl.server/start-server as they are."
-  [port & {:keys [bind transport-fn handler ack-port greeting-fn]}]
+  "Start a network repl for debugging on specified port followed by
+   an optional parameters map. The :bind, :transport-fn, :handler,
+   :ack-port and :greeting-fn will be forwarded to
+   clojure.tools.nrepl.server/start-server as they are."
+  [port & [{:keys [bind transport-fn handler ack-port greeting-fn]}]]
   (if @nrepl-server
     (log/error "nREPL is already running!")
     (try
-      (reset! nrepl-server (nrepl/start-server :port port :bind bind
-                                               :transport-fn transport-fn
-                                               :handler handler :ack-port ack-port
-                                               :greeting-fn greeting-fn))
+      (reset! nrepl-server
+              (nrepl/start-server :port port
+                                  :bind bind
+                                  :transport-fn transport-fn
+                                  :handler handler
+                                  :ack-port ack-port
+                                  :greeting-fn greeting-fn))
       (log/info "nREPL server started on port" port)
       (catch Throwable t
         (log/error t "failed to start nREPL")))))
