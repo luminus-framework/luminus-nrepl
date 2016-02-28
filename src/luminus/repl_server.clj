0851bbb18a39a19081881ba864dd11ed63d06b92
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-28 12:19:09 -0500

    cleanup

--- src/luminus/repl_server.clj
+++ src/luminus/repl_server.clj
@@ -9,15 +9,17 @@
   clojure.tools.nrepl.server/start-server as they are."
   [{:keys [port bind transport-fn handler ack-port greeting-fn]}]
   (try
+    (log/info "starting nREPL server on port" port)
     (nrepl/start-server :port port
                         :bind bind
                         :transport-fn transport-fn
                         :handler handler
                         :ack-port ack-port
                         :greeting-fn greeting-fn)
-    (log/info "nREPL server started on port" port)
+
     (catch Throwable t
-      (log/error t "failed to start nREPL"))))
+      (log/error t "failed to start nREPL")
+      (throw t))))
 
 (defn stop [server]
   (nrepl/stop-server server)
