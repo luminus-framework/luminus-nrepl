a7afd0097df8bfc47df8ad09f69822dbd28d996a
Sotnikov, Dmitri <dmitri.sotnikov@uhn.ca>
2018-07-31 17:13:15 -0400

    bumped up nrepl

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-nrepl "0.1.3"
+(defproject luminus-nrepl "0.1.5"
   :description "nREPL server for Luminus"
   :url "https://github.com/luminus-framework/luminus-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [org.clojure/tools.logging "0.3.1"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [nrepl "0.4.4"]
+                 [org.clojure/tools.logging "0.4.1"]])
