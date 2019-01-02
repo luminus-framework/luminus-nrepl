701b904fce7fa75d47b098d91abb343f15d7ea30
Juan E. Maya <jmayaalv@gmail.com>
2019-01-02 21:18:04 +0100

    upgrade nrepl to the latest version 0.5.3

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [nrepl "0.4.4"]
+                 [nrepl "0.5.3"]
                  [org.clojure/tools.logging "0.4.1"]])
