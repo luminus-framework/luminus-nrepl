d7078a1807937782472ae0626f1eaf8ce1acefcd
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2020-09-14 16:26:39 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-nrepl "0.1.6"
+(defproject luminus-nrepl "0.1.7"
   :description "nREPL server for Luminus"
   :url "https://github.com/luminus-framework/luminus-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [nrepl "0.6.0"]
+                 [nrepl "0.8.1"]
                  [org.clojure/tools.logging "0.4.1"]])
